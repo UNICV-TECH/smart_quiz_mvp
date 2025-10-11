1cfe664f4cf1de9118eeccb8bd9c747291a52057
Debora Rosada <deborarosada12@gmail.com>
2025-10-10 22:59:52 -0300

    Fix: AppColors

--- lib/ui/theme/app_color.dart
+++ lib/ui/theme/app_color.dart
@@ -1,6 +1,68 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  //white
+    static const Color white = Color(0xFFFFFFFF);
+  
+  //green
+    static const Color green1 = Color(0xFF4CAF50);
+    static const Color green2 = Color(0xFF3F8B3A); // branco opaco
+    static const Color accentGreen = Color(0xFF3A6B3F);
+    static const Color green = Color(0xFF3B5C34);
+    static const Color deepGreen = Color(0xFF2F4A2B);
+    static const Color greenmusgo = Color(0xFF516448);
+    static const Color greenmusgoopacidade = Color(0xFF8B9486);
+    static const Color backgroundGradient1 = Color(0xFFEAF5EF);
+    static const Color greenChart = Color.fromARGB(70, 59, 92, 52);
+    static const Color inputLogin = Color.fromARGB(20, 59, 92, 52);
+
+  //black
+    static const Color primaryDark = Color.fromARGB(255, 0, 0, 0);
+    static const Color blackopaco = Color(0x14000000);
+
+  //red
+    static const Color error = Color(0xFFE32F24);
+    static const Color borderColorError = Color(0xFFF44336);
+    static const Color iconRed = Color.fromARGB(255, 213, 44, 52);
+    static const Color red3 = Color(0xFFFF5722);
+    static const Color red = Color(0xFFD9503F);
+    static const Color red2 = Color(0xFFFF5252);
+    static const Color iconBackgroundRed = Color.fromARGB(20, 213, 44, 52);
+
+  //orange
+    static const Color orange = Color(0xFFEF992D);
+    static const Color orangeChart = Color(0xFFDC9B3C);
+
+  //grey
+    static const Color secondaryDark = Color(0xFF666465);
+    static const Color checklistUnselectedText = Color(0xFF616161); // cinza texto
+    static const Color estiloLabel = Color.fromARGB(255, 158, 158, 158);
+    static const Color greyShade400 = Color(0xFFBDBDBD);
+    static const Color tertiaryDark = Color(0xFFccc1c7);
+    static const Color checklistUnselectedBorder = Color(0xFFBDBDBD); // cinza borda
+    static const Color greyShade200 = Color(0xFFEEEEEE);
+
+  //pink
+    static const Color brandButtonBorder = Color.fromARGB(130, 245, 47, 130);
+    static const Color iconBackgroundPink = Color.fromARGB(20, 245, 47, 130);
+
+  //tranparente
+    static const Color shadow = Color(0x1A000000);
+    static const Color transparent = Colors.transparent;
+
+  //blue
+    static const Color borderColorFocus = Color.fromARGB(255, 33, 150, 243);
+    static const Color indigo = Color(0xFF3F51B5);
+    static const Color blueShade = Color(0xFFE3F2FD);
+
+  
+  //yellow
+    static const Color backgroundGradient2 = Color(0xFFF7E9D2);
+
+  static const Color dividerColor = Color(0xFFD3D9CF);
+  static const Color cardFill = Color(0xFFEFF4EA);
+
+
   //grupo web no figma
   static const Color webNeutral100 = Color(0xFFF9F9F9);
   static const Color webNeutral200 = Color(0xFFE6E6E6);
@@ -11,41 +73,4 @@ class AppColors {
   static const Color webNeutral700 = Color(0xFF595959);
   static const Color webNeutral800 = Color(0xFF404040);
   static const Color webNeutral900 = Color(0xFF262626);
-
-  //grupo brand no figma
-  static const Color green = Color(0xFF3B5C34);
-  static const Color greenChart = Color.fromARGB(70, 59, 92, 52);
-  static const Color orange = Color(0xFFEF992D);
-  static const Color orangeChart = Color(0xFFDC9B3C);
-
-  //grupo text no figma
-  static const Color primaryDark = Color.fromARGB(255, 0, 0, 0);
-  static const Color secondaryDark = Color(0xFF666465);
-  static const Color tertiaryDark = Color(0xFFccc1c7);
-
-  static const Color error = Color(0xFFE32F24);
-
-  static const Color white = Color(0xFFFFFFFF);
-
-  //background colors
-  static const Color background = Color(0xFFF4F4F4);
-  static const Color iconBackgroundPink = Color.fromARGB(20, 245, 47, 130);
-  static const Color brandButtonBorder = Color.fromARGB(130, 245, 47, 130);
-  static const Color iconBackgroundRed = Color.fromARGB(20, 213, 44, 52);
-  static const Color iconRed = Color.fromARGB(255, 213, 44, 52);
-  static const Color shadow = Color(0x1A000000);
-  static const Color inputLogin = Color.fromARGB(20, 59, 92, 52);
-
-  // checklist colors
-  static const Color checklistSelected = Color(0xFF53B175); // verde seleção
-  static const Color checklistUnselectedBorder = Color(0xFFBDBDBD); // cinza borda
-  static const Color checklistUnselectedText = Color(0xFF616161); // cinza texto
-
-  // Transparent color for UI elements
-  static const Color transparent = Colors.transparent;
-
-  //textfield color
-  static const Color borderColorFocus = Color.fromARGB(255, 33, 150, 243);
-  static const Color borderColorError = Color.fromARGB(255, 244, 67, 54);
-  static const Color estiloLabel = Color.fromARGB(255, 158, 158, 158);
 }
