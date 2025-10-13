73da6ee9bce2c936e72e51bf5460e058a8aa8477
Gabriella Freitas <gsf.gabriellasantos@gmail.com>
2025-10-13 11:36:06 -0300

    feat: adiciona arquivo de strings centralizadas e telas iniciais
    - Cria AppStrings para centralizar strings do aplicativo
    - Adiciona splash_screen.dart e welcome_screen.dart
    - Inclui assets de imagens (background.jpg e logo.webp)
    - Organiza estrutura de pastas seguindo convenÃ§Ãµes Flutter

---
+++ lib/constants/app_strings.dart
@@ -0,0 +1,7 @@
+class AppStrings {
+  // Tela de Boas-vindas
+  static const String welcomeTitle = 'Bem vindo';
+  static const String welcomeSubtitle = 'Prepare-se para se divertir aprendendo!';
+  static const String enterButton = 'Entrar';
+  
+}
---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,103 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../ui/theme/app_color.dart';
+import 'welcome_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+  
+  final int _splashDuration = 2500; 
+
+  @override
+  void initState() {
+    super.initState();
+    
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500), 
+    );
+    
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut, // Curva suave
+      ),
+    );
+    
+    _animationController.forward();
+    
+    Timer(Duration(milliseconds: _splashDuration), () {
+      _navigateToNextScreen();
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _navigateToNextScreen() async {
+    if (mounted) {
+      _animationController.reverse();
+      
+      Navigator.of(context).pushReplacement(
+        PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) {
+            return const WelcomeScreen();
+          },
+          transitionDuration: const Duration(milliseconds: 1000),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: CurvedAnimation(
+                parent: animation,
+                curve: Curves.easeInOut,
+              ),
+              child: child,
+            );
+          },
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.green,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          color: Color(0xFF4D7B58), 
+        ),
+        child: Center(
+          child: FadeTransition(
+            opacity: _animation,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Logo do app
+                Image.asset(
+                  'assets/images/logo.webp',
+                  width: 300,
+                  height: 300,
+                ),
+                const SizedBox(height: 24)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
