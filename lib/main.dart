c448a80124435b79eca114d800eeae58efb4c671
Joao Pedro Romano <jpvs06x0805@gmail.com>
2025-10-28 16:19:36 -0300

    fix: correção main, versão anterior estava com uma main temporaria para testar a tela

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:unicv_tech_mvp/ui/theme/app_color.dart';
-import 'package:unicv_tech_mvp/views/QuizConfig_screen.dart';
-// 1. Mantém o import da tela que queremos testar
-
-
-// Imports das outras telas foram removidos para simplificar.
+import 'package:unicv_tech_mvp/views/reset_password_screen1.dart';
+import 'package:unicv_tech_mvp/views/reset_password_screen2.dart';
+import 'views/splash_screen.dart';
+import 'views/welcome_screen.dart';
+import 'views/signup_screen.dart';
+import 'views/login_screen.dart';
+import 'views/profile_screen.dart';
+import 'views/main_navigation_screen.dart';
+import 'views/help_screen.dart';
+import 'views/about_screen.dart';
+import 'ui/theme/app_color.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,13 +20,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // 3. Cria um curso "fictício" para passar para a tela de teste
-    final Map<String, dynamic> testCourse = {
-      'id': 'teste_curso',
-      'title': 'Curso de Teste',
-      'icon': Icons.science_outlined // Um ícone qualquer
-    };
-
     return MaterialApp(
       title: 'UniCV Tech',
       debugShowCheckedModeBanner: false,
@@ -30,12 +28,69 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         fontFamily: 'Poppins',
       ),
-      // 4. Inicia o app diretamente na QuizConfigScreen, passando o curso de teste
-      home: QuizConfigScreen(course: testCourse), 
-      
-      // 5. O mapa de rotas foi removido.
-      // routes: { ... },
+      home: const SplashScreen(),
+      routes: {
+        '/splash': (context) => const SplashScreen(),
+        '/welcome': (context) => const WelcomeScreen(),
+        '/signup': (context) => const SignupScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/reset_password': (context) => const ResetPasswordScreen1(),
+        '/reset_password2': (context) => const ResetPasswordScreen2(),
+        '/main': (context) => const MainNavigationScreen(),
+        '/profile': (context) => const ProfileScreen(),
+        '/help': (context) => const HelpScreen(),
+        '/about': (context) => const AboutScreen(),
+        '/home': (context) =>
+            const Scaffold(body: Center(child: Text('Tela Principal'))),
+      },
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
