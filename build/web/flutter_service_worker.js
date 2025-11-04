60d8beb091bbddec9643372348a677c96fe61b08
Gustavo <gustavo96ma@gmail.com>
2025-11-04 16:26:01 -0300

    fix: update service worker version and resource hashes for build consistency

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ab398fe51212087e827ea719ff276a41",
+const RESOURCES = {"flutter_bootstrap.js": "f150c8518cd04042fe9294643607b7d6",
 "version.json": "48dbe1dd505e72b7682d698f38de93f2",
-"index.html": "678dfd21563dbabd9aab0b3105b595d3",
-"/": "678dfd21563dbabd9aab0b3105b595d3",
+"index.html": "cf4db0db2ed7180cda112577d0564338",
+"/": "cf4db0db2ed7180cda112577d0564338",
 "main.dart.js": "1ef6581f16fd4ee8b09a1fccdf7626e0",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
