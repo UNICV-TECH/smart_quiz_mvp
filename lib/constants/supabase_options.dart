ed125f68d1e332e6f88dacf2e1f4a0e9d041e15c
Gustavo <gustavo96ma@gmail.com>
2025-11-04 16:38:57 -0300

    feat: add Supabase configuration files and update launch settings

--- lib/constants/supabase_options.dart
+++ lib/constants/supabase_options.dart
@@ -1,9 +1,7 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 class SupabaseOptions {
   SupabaseOptions._();
-  static String get url => dotenv.env['SUPABASE_URL'] ?? '';
-  static String get anonKey => dotenv.env['SUPABASE_ANON_KEY'] ?? '';
+  static String get url => String.fromEnvironment('SUPABASE_URL');
+  static String get anonKey => String.fromEnvironment('SUPABASE_ANON_KEY');
 
   static bool get isConfigured => url.isNotEmpty && anonKey.isNotEmpty;
 }
