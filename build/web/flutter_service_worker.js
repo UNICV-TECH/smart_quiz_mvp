72f6d2d6beb89a9309becf9e3c62f89b93861901
Debora Rosada <deborarosada12@gmail.com>
2025-11-06 21:33:07 -0300

    Refactor code structure for improved readability and maintainability

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -30,14 +30,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "f6574d459aca180b3d9ebd1a840b2d3f
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "ef95caaaeb2c02b6441ffb73c9f41f1d",
+"flutter_bootstrap.js": "577bcaaf7e64a1b8cae06b5bca518566",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1818f1d37f7808b9be45f4e046098405",
-"/": "1818f1d37f7808b9be45f4e046098405",
-"main.dart.js": "fbeed1fd5438294bbabb4e8626578a84",
+"index.html": "b0b65a7343d8c1d2971d70e81aa6f7a5",
+"/": "b0b65a7343d8c1d2971d70e81aa6f7a5",
+"main.dart.js": "111b3e00b5fbc0c783e196983bc7a6ce",
 "manifest.json": "15df426d07b9383f185e699cc145d67b",
 "version.json": "48dbe1dd505e72b7682d698f38de93f2"};
 // The application shell files that are downloaded before a service worker can
