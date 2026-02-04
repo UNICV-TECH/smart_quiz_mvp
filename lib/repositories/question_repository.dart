0df05f545c04ff823629c9fb28051d4e197ad900
Gustavo <gustavo96ma@gmail.com>
2026-02-04 17:48:08 -0300

    refactor: remove unused fetchQuestions method from QuestionRepository and SupabaseQuestionRepository

--- lib/repositories/question_repository.dart
+++ lib/repositories/question_repository.dart
@@ -1,11 +1,6 @@
 import 'question_repository_types.dart';
 
 abstract class QuestionRepository {
-  Future<List<Question>> fetchQuestions({
-    required String examId,
-    required int limit,
-  });
-
   Future<List<Question>> fetchQuestionsByIds({
     required List<String> questionIds,
   });
